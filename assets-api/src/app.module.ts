6a6bc176734447ccc1f1816cee6b340acfed931a
benjaaguila <benjaaguilaruiz@uc.cl>
2024-07-11 11:43:46 -0400

    feat: TypeORM setUP added

--- assets-api/src/app.module.ts
+++ assets-api/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const DB_PORT = process.env.DB_PORT || '5432';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [], // add entities here
+      synchronize: true,
+    }),
+    // add other modules here
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
