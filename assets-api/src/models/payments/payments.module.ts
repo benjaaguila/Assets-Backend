863b4022088ac71a05d84d23a6c57749af97c059
benjaaguila <benjaaguilaruiz@uc.cl>
2024-07-12 13:31:43 -0400

    feat: ClientsModule and ManagersModule imported

--- assets-api/src/models/payments/payments.module.ts
+++ assets-api/src/models/payments/payments.module.ts
@@ -3,9 +3,14 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { ClientsModule } from '../clients/clients.module';
+import { ManagersModule } from '../managers/managers.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment]),
+    ClientsModule,
+    ManagersModule,
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
