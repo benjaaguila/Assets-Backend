c141590502538f45108d3a331d83a0d77f611013
benjaaguila <benjaaguilaruiz@uc.cl>
2024-07-12 13:39:11 -0400

    feat: findPaymentsInfoByClientName added

--- assets-api/src/models/payments/payments.service.ts
+++ assets-api/src/models/payments/payments.service.ts
@@ -3,12 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Payment } from './entities/payment.entity';
 import { CreatePaymentDto } from './dto/payment.dto';
+import { ClientsService } from '../clients/clients.service';
+import { ManagersService } from '../managers/managers.service';
 
 @Injectable()
 export class PaymentsService {
   constructor(
     @InjectRepository(Payment)
     private paymentRepository: Repository<Payment>,
+    private clientsService: ClientsService,
+    private managersService: ManagersService,
   ) {}
 
   async findAllPayments(): Promise<Payment[]> {
@@ -26,6 +30,50 @@ export class PaymentsService {
 
   private async getPaymentsCount(): Promise<number> {
     return await this.paymentRepository.count();
+  }
+
+  async findPaymentsInfoByClientName(clientName: string): Promise<{ managerName: string, totalAmountManaged: number }[]> {
+    const client = await this.clientsService.findOneClientByName(clientName);
+    const managerIds = [...new Set((await this.findPaymentsByClientId(client.clientId)).map((payment) => payment.managerId))];
+    const managerInfo = await Promise.all(managerIds.map((managerId) => this.getManagerInfo(managerId, client.clientId)));
+
+    return managerInfo;
+}
+
+
+  async findPaymentsByClientId(clientId: string): Promise<Payment[]> {
+    return await this.paymentRepository.find({
+      where: {
+        clientId,
+      },
+    });
+  }
+
+  async getManagerInfo(managerId: string, clientId: string): Promise<{ managerName: string, totalAmountManaged: number }> {
+    const manager = await this.managersService.findOneManager(managerId);
+    const totalAmountManaged = await this.calculateTotalAmountByManagerId(clientId, managerId);
+    return {
+        managerName: manager.name,
+        totalAmountManaged,
+    };
+}
+
+private async calculateTotalAmountByManagerId(clientId: string, managerId: string): Promise<number> {
+  const payments = await this.findPaymentsByClientIdAndManagerId(clientId, managerId);
+
+  const totalAmount: number = payments.reduce((total, payment) => total + Number(payment.amount), 0);
+
+  return totalAmount;
+}
+
+
+  async findPaymentsByClientIdAndManagerId(clientId: string, managerId: string): Promise<Payment[]> {
+    return await this.paymentRepository.find({
+      where: {
+        clientId,
+        managerId,
+      },
+    });
   }
 
 }
